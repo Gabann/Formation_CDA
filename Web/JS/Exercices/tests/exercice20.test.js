9d7b1b14ee53228067fc15adf3064fb2118f9dbb
Gaban <gabin.deboulogne@gmail.com>
2023-12-01 16:43:17 +0100

    Restructured project structure
    Started tp pokeapi

--- Web/JS/Exercices/tests/exercice20.test.js
+++ Web/JS/Exercices/tests/exercice20.test.js
@@ -1,32 +1,32 @@
-const {getProportion, verifyInput} = require('../exercice20');
+const {getProportion, verifyInput} = require('../exercice20.js');
 
 test('Test if the adn percenteage is correct', () => {
 	expect(getProportion("act", "gtc")).toBe(0);
 	expect(getProportion("acac", "acac")).toBe(1);
-	expect(getProportion("actgtagt", "ac")).toBe(2/8);
-	expect(getProportion("actgtagt", "tgt")).toBe(3/8);
-	expect(getProportion("actgactgaa", "actgactg")).toBe(8/10);
-	
+	expect(getProportion("actgtagt", "ac")).toBe(2 / 8);
+	expect(getProportion("actgtagt", "tgt")).toBe(3 / 8);
+	expect(getProportion("actgactgaa", "actgactg")).toBe(8 / 10);
+
 	//Checks for sequence repeating multiple times but with non-matching sequence in the middle
-	expect(getProportion("acatac", "ac")).toBe(4/6);
-	
+	expect(getProportion("acatac", "ac")).toBe(4 / 6);
+
 	//Checks for case insensitivity
-	expect(getProportion("ACaTaC", "ac")).toBe(4/6);
-	
+	expect(getProportion("ACaTaC", "ac")).toBe(4 / 6);
+
 	//Checks for whitespaces removal
-	expect(getProportion("   actgactgaa  ", "actgactg")).toBe(8/10);
-	expect(getProportion(" actgactgaa", "actgactg   ")).toBe(8/10);
+	expect(getProportion("   actgactgaa  ", "actgactg")).toBe(8 / 10);
+	expect(getProportion(" actgactgaa", "actgactg   ")).toBe(8 / 10);
 	expect(getProportion("   act   ", "gtc")).toBe(0);
-	expect(getProportion("actga ctgaa", "actga")).toBe(5/10);
+	expect(getProportion("actga ctgaa", "actga")).toBe(5 / 10);
 });
 
 test('Test is the input is valid', () => {
 	expect(verifyInput("actgactgaa")).toBe(true);
 	expect(verifyInput("acatac")).toBe(true);
-	
+
 	//Checks for case insensitivity
 	expect(verifyInput("ACTgtagT")).toBe(true);
-	
+
 	//Checks for whitespace removal
 	expect(verifyInput("   actgactgaa")).toBe(true);
 	expect(verifyInput("actgac   tgaa")).toBe(true);
---
+++ Web/TP PokeAPI/js/main.js
@@ -0,0 +1,94 @@
+import {Pokemon} from "./pokemon.js";
+
+const baseUrl = "https://pokeapi.co/api/v2/";
+
+let searchButton = document.querySelector(".searchbar button");
+let previousButton = document.querySelector("#previous");
+let nextButton = document.querySelector("#next");
+let currentPokemon;
+
+async function getPokemonByNameOrId(name) {
+	try {
+		const response = await fetch(baseUrl + "pokemon/" + name);
+		const data = await response.json();
+
+		//Used to get 4 randoms moves the pokemon can learn to get a complete move set
+		let shuffledMoves = [...data.moves].sort(() => 0.5 - Math.random());
+		let moveSet = shuffledMoves.slice(0, 4);
+
+		currentPokemon = new Pokemon(data.id, data.sprites, data.name, data.height, data.weight, data.types, moveSet);
+		return currentPokemon;
+
+	} catch (e) {
+		console.error(e);
+	}
+}
+
+async function displayPokemon(pokemonName) {
+	try {
+		let pokemon = await getPokemonByNameOrId(pokemonName);
+
+		document.querySelector(".pokemon-card").innerHTML = `
+		<div class="row text-center" style="height: 80%">
+			<div class="col-6">
+				<div class="pokemon-img text-center"  style="height: 100%">
+					<img src=${pokemon.sprite.front_default}  style="height: 100%"></img>
+				</div>
+			</div>
+			<div class="col-6">
+				<div class="pokemon-name">
+					${pokemon.name}
+				</div>
+				<div class="row">
+					<div class="pokemon-height col-6">
+						${pokemon.height}
+					</div>
+				<div class="pokemon-weight col-6">
+					${pokemon.weight}
+				</div>
+			</div>
+
+			<div class="pokemon-types"></div>
+			</div>
+		</div>
+
+		<div class="row pokemon-moves"></div>`;
+
+		pokemon.types.forEach((type) => {
+			document.querySelector(".pokemon-types").innerHTML += type.type.name + " ";
+		});
+
+		pokemon.moves.forEach((move) => {
+			document.querySelector(".pokemon-moves").innerHTML += move.move.name + " ";
+		});
+	} catch (e) {
+		console.error(e);
+	}
+}
+
+searchButton.addEventListener("click", () => {
+	displayPokemon(document.querySelector("#searchbar-input").value);
+});
+
+previousButton.addEventListener("click", () => {
+	displayPokemon(currentPokemon.id - 1);
+});
+
+nextButton.addEventListener("click", () => {
+	displayPokemon(currentPokemon.id + 1);
+});
+
+displayPokemon(79);
+
+async function getPokedexByGeneration(generationNumber) {
+
+	try {
+		const response = await fetch(baseUrl + "generation/" + generationNumber);
+		const data = await response.json();
+		console.log(data.);
+	} catch (e) {
+		console.error(e);
+	}
+}
+
+getPokedexByGeneration(1);
