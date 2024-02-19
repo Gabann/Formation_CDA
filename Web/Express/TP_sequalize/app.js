410340b058e934470245abfe39276ac242f22794
Gaban <gabin.deboulogne@gmail.com>
2024-02-19 09:28:56 +0100

    Installed React

--- Web/Express/TP_sequalize/app.js
+++ Web/Express/TP_sequalize/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = require("./routes/router.js");
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use('/', router);
 
---
+++ Web/React/src/App.tsx
@@ -0,0 +1,15 @@
+import './global.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min'
+import {TestComponent} from "./components/TestComponent.tsx";
+
+function App() {
+    return (
+        <div>
+            <TestComponent>
+            </TestComponent>
+        </div>
+    )
+}
+
+export default App
