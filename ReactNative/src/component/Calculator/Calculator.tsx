5f583d2063ed83c41c0067685ba5b9c5f73c2149
Gaban <gabin.deboulogne@gmail.com>
2024-03-13 11:53:51 +0100

    Way easier and more powerful calculator using eval() function

--- ReactNative/src/component/Calculator/Calculator.tsx
+++ ReactNative/src/component/Calculator/Calculator.tsx
@@ -4,8 +4,7 @@ import {CalculatorButton} from "./CalculatorButton.tsx";
 
 type State = {
 	resultValue: number,
-	displayValue: number,
-	mode: string
+	expression: string[]
 };
 
 let calculatorMode = {
@@ -20,103 +19,30 @@ let calculatorMode = {
 export class Calculator extends Component<{}, State> {
 	state = {
 		resultValue: 0,
-		displayValue: 0,
-		mode: '',
+		expression: []
 	};
-
-	calculate = async (): Promise<void> => {
-		let result = this.state.resultValue;
-
-		switch (this.state.mode) {
-			case calculatorMode.add:
-				result += this.state.displayValue;
-				break;
-			case calculatorMode.subtract:
-				result -= this.state.displayValue;
-				break;
-			case calculatorMode.divide:
-				result /= this.state.displayValue;
-				break;
-			case calculatorMode.multiply:
-				result *= this.state.displayValue;
-				break;
-			case calculatorMode.power:
-				result = Math.pow(result, this.state.displayValue);
-				break;
-			case calculatorMode.percent:
-				result /= 100;
-				break;
-			default:
-				result = this.state.displayValue;
-		}
-
-		await this.setState({resultValue: result});
-	}
-
-	addNumberToDisplay = (number: number): void => {
-		this.setState({
-			displayValue: Number(String(this.state.displayValue) + Number(String(number)))
-		})
-	}
-
-	removeLastNumber = (): void => {
-		this.setState({displayValue: Number(String(this.state.displayValue).slice(0, -1))});
-	}
-
-	dot = (): void => {
-		this.setState({displayValue: Number(String(this.state.displayValue) + '.')});
-	}
-
-	setMode = (mode: string): void => {
-		this.setState({
-			mode,
-			resultValue: this.state.displayValue,
-			displayValue: 0
+	
+	addToExpression = (value: string): void => {
+		this.setState({expression: [...this.state.expression, value]}, () => {
+			this.showResult();
 		});
-	};
-
-	add = (): void => {
-		this.setMode(calculatorMode.add);
-	};
-
-	subtract = (): void => {
-		this.setMode(calculatorMode.subtract);
-	};
-
-	divide = (): void => {
-		this.setMode(calculatorMode.divide);
-	};
-
-	multiply = (): void => {
-		this.setMode(calculatorMode.multiply);
-	};
-
-	percent = (): void => {
-		this.setMode(calculatorMode.percent);
-		this.equal();
-	};
-
-	power = (): void => {
-		this.setMode(calculatorMode.power);
-	};
-
-	equal = async (): Promise<void> => {
-		await this.calculate();
-		this.showResult();
+	}
+	reset = (): void => {
+		this.setState({resultValue: 0, expression: []});
 	}
 
-	reset = (): void => {
-		this.setState({resultValue: 0, displayValue: 0, mode: ''});
+	removeLastExpression = (): void => {
+		this.setState({expression: this.state.expression.slice(0, -1)}, () => {
+			this.showResult();
+		});
 	}
 
 	showResult = (): void => {
-		this.setState({displayValue: this.state.resultValue});
-	}
-
-	debug = (): void => {
-		console.log(`result: ${this.state.resultValue}`);
-		console.log(`displayNumber: ${this.state.displayValue}`);
-		console.log(`mode: ${this.state.mode}`);
+		try {
+			this.setState({resultValue: eval(this.state.expression.join(''))});
+		} catch (error) {
+			console.error(error);
+		}
 	}
 
 	render() {
@@ -126,47 +52,98 @@ export class Calculator extends Component<{}, State> {
 				<Text style={styles.title}>Calculator</Text>
 
 				<View>
-					<Text style={styles.result}>{this.state.displayValue}</Text>
+					<Text style={styles.result}>{this.state.expression}</Text>
+
+					{this.state.expression.length > 0 ? (
+						<Text style={styles.result}>={this.state.resultValue}</Text>
+					) : (
+						<Text style={styles.result}>0</Text>
+					)}
 				</View>
 
 				<View style={styles.buttons}>
 					<View style={{flexDirection: 'row'}}>
 						<CalculatorButton action={this.reset} text={"AC"} style={"Dark"}></CalculatorButton>
-						<CalculatorButton action={this.power} text={"^"} style={"Dark"}></CalculatorButton>
-						<CalculatorButton action={this.percent} text={"%"} style={"Dark"}></CalculatorButton>
-						<CalculatorButton action={this.divide} text={"/"} style={"Dark"}></CalculatorButton>
+
+						<CalculatorButton action={() => {
+							this.addToExpression('**')
+						}} text={"^"} style={"Dark"}></CalculatorButton>
+
+						<CalculatorButton action={() => {
+							this.addToExpression('/100')
+						}} text={"%"} style={"Dark"}></CalculatorButton>
+
+						<CalculatorButton action={() => {
+							this.addToExpression('/')
+						}} text={"/"} style={"Dark"}></CalculatorButton>
 					</View>
 
 					<View style={{flexDirection: 'row'}}>
-						<CalculatorButton action={() => this.addNumberToDisplay(7)} text={"7"} style={"Light"}></CalculatorButton>
-						<CalculatorButton action={() => this.addNumberToDisplay(8)} text={"8"} style={"Light"}></CalculatorButton>
-						<CalculatorButton action={() => this.addNumberToDisplay(9)} text={"9"} style={"Light"}></CalculatorButton>
-						<CalculatorButton action={this.multiply} text={"*"} style={"Dark"}></CalculatorButton>
+						<CalculatorButton action={() => {
+							this.addToExpression('7')
+						}} text={"7"} style={"Dark"}></CalculatorButton>
+
+						<CalculatorButton action={() => {
+							this.addToExpression('8')
+						}} text={"8"} style={"Dark"}></CalculatorButton>
+
+						<CalculatorButton action={() => {
+							this.addToExpression('9')
+						}} text={"9"} style={"Dark"}></CalculatorButton>
+
+						<CalculatorButton action={() => {
+							this.addToExpression('*')
+						}} text={"*"} style={"Dark"}></CalculatorButton>
 					</View>
 
 					<View style={{flexDirection: 'row'}}>
-						<CalculatorButton action={() => this.addNumberToDisplay(4)} text={"4"} style={"Light"}></CalculatorButton>
-						<CalculatorButton action={() => this.addNumberToDisplay(5)} text={"5"} style={"Light"}></CalculatorButton>
-						<CalculatorButton action={() => this.addNumberToDisplay(6)} text={"6"} style={"Light"}></CalculatorButton>
-						<CalculatorButton action={this.subtract} text={"-"} style={"Dark"}></CalculatorButton>
+						<CalculatorButton action={() => {
+							this.addToExpression('4')
+						}} text={"4"} style={"Dark"}></CalculatorButton>
+
+						<CalculatorButton action={() => {
+							this.addToExpression('5')
+						}} text={"5"} style={"Dark"}></CalculatorButton>
+
+						<CalculatorButton action={() => {
+							this.addToExpression('6')
+						}} text={"6"} style={"Dark"}></CalculatorButton>
+
+						<CalculatorButton action={() => {
+							this.addToExpression('-')
+						}} text={"-"} style={"Dark"}></CalculatorButton>
 					</View>
 
 					<View style={{flexDirection: 'row'}}>
-						<CalculatorButton action={() => this.addNumberToDisplay(1)} text={"1"} style={"Light"}></CalculatorButton>
-						<CalculatorButton action={() => this.addNumberToDisplay(2)} text={"2"} style={"Light"}></CalculatorButton>
-						<CalculatorButton action={() => this.addNumberToDisplay(3)} text={"3"} style={"Light"}></CalculatorButton>
-						<CalculatorButton action={this.add} text={"+"} style={"Dark"}></CalculatorButton>
+						<CalculatorButton action={() => {
+							this.addToExpression('1')
+						}} text={"1"} style={"Dark"}></CalculatorButton>
+
+						<CalculatorButton action={() => {
+							this.addToExpression('2')
+						}} text={"2"} style={"Dark"}></CalculatorButton>
+
+						<CalculatorButton action={() => {
+							this.addToExpression('3')
+						}} text={"3"} style={"Dark"}></CalculatorButton>
+
+						<CalculatorButton action={() => {
+							this.addToExpression('+')
+						}} text={"+"} style={"Dark"}></CalculatorButton>
 					</View>
 
 					<View style={{flexDirection: 'row'}}>
-						<CalculatorButton action={this.dot} text={"."} style={"Light"}></CalculatorButton>
-						<CalculatorButton action={() => this.addNumberToDisplay(0)} text={"0"} style={"Light"}></CalculatorButton>
-						<CalculatorButton action={this.removeLastNumber} text={"Del"} style={"Light"}></CalculatorButton>
-						<CalculatorButton action={this.equal} text={"="} style={"Dark"}></CalculatorButton>
-					</View>
+						<CalculatorButton action={() => {
+							this.addToExpression('.')
+						}} text={"."} style={"Dark"}></CalculatorButton>
 
-					<View style={{flexDirection: 'row'}}>
-						<CalculatorButton action={this.debug} text={"Debug"} style={"Light"}></CalculatorButton>
+						<CalculatorButton action={() => {
+							this.addToExpression('0')
+						}} text={"0"} style={"Dark"}></CalculatorButton>
+
+
+						<CalculatorButton action={this.removeLastExpression} text={"Del"} style={"Light"}></CalculatorButton>
+						<CalculatorButton action={this.showResult} text={"="} style={"Dark"}></CalculatorButton>
 					</View>
 				</View>
 			</View>
