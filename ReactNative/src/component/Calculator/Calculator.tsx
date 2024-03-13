c5631bffd45a0386d48ad00df89eeea13f720210
Gaban <gabin.deboulogne@gmail.com>
2024-03-13 12:25:14 +0100

    Removed unused var

--- ReactNative/src/component/Calculator/Calculator.tsx
+++ ReactNative/src/component/Calculator/Calculator.tsx
@@ -7,21 +7,12 @@ type State = {
 	expression: string[]
 };
 
-let calculatorMode = {
-	add: '+',
-	subtract: '-',
-	divide: '/',
-	multiply: '*',
-	power: '^',
-	percent: '%'
-}
-
 export class Calculator extends Component<{}, State> {
 	state = {
 		resultValue: 0,
 		expression: []
 	};
-	
+
 	addToExpression = (value: string): void => {
 		this.setState({expression: [...this.state.expression, value]}, () => {
 			this.showResult();
@@ -63,87 +54,87 @@ export class Calculator extends Component<{}, State> {
 
 				<View style={styles.buttons}>
 					<View style={{flexDirection: 'row'}}>
-						<CalculatorButton action={this.reset} text={"AC"} style={"Dark"}></CalculatorButton>
+						<CalculatorButton action={this.reset} text={"AC"} style={"grey"}></CalculatorButton>
 
 						<CalculatorButton action={() => {
 							this.addToExpression('**')
-						}} text={"^"} style={"Dark"}></CalculatorButton>
+						}} text={"^"} style={"grey"}></CalculatorButton>
 
 						<CalculatorButton action={() => {
 							this.addToExpression('/100')
-						}} text={"%"} style={"Dark"}></CalculatorButton>
+						}} text={"%"} style={"grey"}></CalculatorButton>
 
 						<CalculatorButton action={() => {
 							this.addToExpression('/')
-						}} text={"/"} style={"Dark"}></CalculatorButton>
+						}} text={"/"} style={"grey"}></CalculatorButton>
 					</View>
 
 					<View style={{flexDirection: 'row'}}>
 						<CalculatorButton action={() => {
 							this.addToExpression('7')
-						}} text={"7"} style={"Dark"}></CalculatorButton>
+						}} text={"7"} style={"light"}></CalculatorButton>
 
 						<CalculatorButton action={() => {
 							this.addToExpression('8')
-						}} text={"8"} style={"Dark"}></CalculatorButton>
+						}} text={"8"} style={"light"}></CalculatorButton>
 
 						<CalculatorButton action={() => {
 							this.addToExpression('9')
-						}} text={"9"} style={"Dark"}></CalculatorButton>
+						}} text={"9"} style={"light"}></CalculatorButton>
 
 						<CalculatorButton action={() => {
 							this.addToExpression('*')
-						}} text={"*"} style={"Dark"}></CalculatorButton>
+						}} text={"*"} style={"grey"}></CalculatorButton>
 					</View>
 
 					<View style={{flexDirection: 'row'}}>
 						<CalculatorButton action={() => {
 							this.addToExpression('4')
-						}} text={"4"} style={"Dark"}></CalculatorButton>
+						}} text={"4"} style={"light"}></CalculatorButton>
 
 						<CalculatorButton action={() => {
 							this.addToExpression('5')
-						}} text={"5"} style={"Dark"}></CalculatorButton>
+						}} text={"5"} style={"light"}></CalculatorButton>
 
 						<CalculatorButton action={() => {
 							this.addToExpression('6')
-						}} text={"6"} style={"Dark"}></CalculatorButton>
+						}} text={"6"} style={"light"}></CalculatorButton>
 
 						<CalculatorButton action={() => {
 							this.addToExpression('-')
-						}} text={"-"} style={"Dark"}></CalculatorButton>
+						}} text={"-"} style={"grey"}></CalculatorButton>
 					</View>
 
 					<View style={{flexDirection: 'row'}}>
 						<CalculatorButton action={() => {
 							this.addToExpression('1')
-						}} text={"1"} style={"Dark"}></CalculatorButton>
+						}} text={"1"} style={"light"}></CalculatorButton>
 
 						<CalculatorButton action={() => {
 							this.addToExpression('2')
-						}} text={"2"} style={"Dark"}></CalculatorButton>
+						}} text={"2"} style={"light"}></CalculatorButton>
 
 						<CalculatorButton action={() => {
 							this.addToExpression('3')
-						}} text={"3"} style={"Dark"}></CalculatorButton>
+						}} text={"3"} style={"light"}></CalculatorButton>
 
 						<CalculatorButton action={() => {
 							this.addToExpression('+')
-						}} text={"+"} style={"Dark"}></CalculatorButton>
+						}} text={"+"} style={"grey"}></CalculatorButton>
 					</View>
 
 					<View style={{flexDirection: 'row'}}>
 						<CalculatorButton action={() => {
 							this.addToExpression('.')
-						}} text={"."} style={"Dark"}></CalculatorButton>
+						}} text={"."} style={"light"}></CalculatorButton>
 
 						<CalculatorButton action={() => {
 							this.addToExpression('0')
-						}} text={"0"} style={"Dark"}></CalculatorButton>
+						}} text={"0"} style={"light"}></CalculatorButton>
 
 
-						<CalculatorButton action={this.removeLastExpression} text={"Del"} style={"Light"}></CalculatorButton>
-						<CalculatorButton action={this.showResult} text={"="} style={"Dark"}></CalculatorButton>
+						<CalculatorButton action={this.removeLastExpression} text={"Del"} style={"light"}></CalculatorButton>
+						<CalculatorButton action={this.showResult} text={"="} style={"grey"}></CalculatorButton>
 					</View>
 				</View>
 			</View>
