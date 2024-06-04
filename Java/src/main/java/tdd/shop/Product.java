531ff8e7f0b4352a7fddb93c1553ca0fa58c5b56
Gaban <gabin.deboulogne@gmail.com>
2024-06-04 11:12:45 +0200

    passed test cases

--- Java/src/main/java/tdd/shop/Product.java
+++ Java/src/main/java/tdd/shop/Product.java
@@ -16,12 +16,26 @@ public class Product implements ProductInterface
 
 	public void setQuality(int quality)
 	{
-
+		this.quality = quality;
+		if (this.quality < minQuality)
+		{
+			this.quality = minQuality;
+		}
+		else if (this.quality > maxQuality)
+		{
+			this.quality = maxQuality;
+		}
 	}
 
 	@Override
 	public void updateProduct()
 	{
+		this.daysLeftToSell--;
+		this.quality--;
 
+		if (this.type.equalsIgnoreCase("dairy"))
+		{
+			this.quality--;
+		}
 	}
 }
